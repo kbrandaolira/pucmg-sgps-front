5768e497cf454c0a15df4b951fb184dab253c489
Kayan Lira <kbrandaolira@gmail.com>
2022-01-19 09:35:02 -0300

    form de cadastro de associado ok

--- src/components/Associates/AssociateForm.js
+++ src/components/Associates/AssociateForm.js
@@ -1,36 +1,101 @@
 import React, { Component } from 'react';
-import {Container, Button, Form} from 'react-bootstrap'
+import {Container, Button, Form, Row, Col} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 class AssociateForm extends Component {
   render() {
     return (
         <>
             <Container>
-                <h4>Cadastrar associado</h4>
+                <h4>Cadastrar Associado</h4>
                 <hr></hr>
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridNome">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control type="text" placeholder="Digite o nome" />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
+                        <Form.Group as={Col} controlId="formGridDataNascimento">
+                        <Form.Label>Data de Nascimento</Form.Label>
+                        <Form.Control type="text" placeholder="Digite a data de nascimento" />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridCPF">
+                        <Form.Label>CPF</Form.Label>
+                        <Form.Control type="text" placeholder="Digite o CPF" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>E-mail</Form.Label>
+                        <Form.Control type="email" placeholder="Digite a e-mail" />
+                        </Form.Group>
+                    </Row>
+
+                    <hr/>
+
+                    <Row className="mb-3">
+                        <div key='inline-radio' className="mb-3">
+                            <Form.Check
+                                inline
+                                label="Empresarial"
+                                name="tipoPlano"
+                                type='radio'
+                                id='inline-radio-plano-empresarial'
+                            />
+                            <Form.Check
+                                inline
+                                label="Individual"
+                                name="tipoPlano"
+                                type='radio'
+                                id='inline-radio-plano-individual'
+                            />
+                        </div>
+
+                        <Form.Group as={Col} controlId="formGridCNPJ">
+                            <Form.Label>CNPJ da Empresa</Form.Label>
+                            <Form.Control type="text" placeholder="Digite o CNPJ da empresa" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridClassificacao">    
+                            <Form.Label>Classificação</Form.Label>
+                            <Form.Select defaultValue="Selecione">
+                                <option value="">Selecione</option>
+                                <option value="ENFERMARIA">Enfermaria</option>
+                                <option value="APARTAMENTO">Apartamento</option>
+                                <option value="VIP">VIP</option>
+                            </Form.Select>
+                        </Form.Group>
+                        
+                        <Form.Group as={Col} controlId="formGridPlanoOdontologico">  
+                            <Form.Label>Plano Odontológico?</Form.Label>
+                            <div key='inline-radio' className="mb-3">
+                                <Form.Check
+                                    inline
+                                    label="Sim"
+                                    name="planoOdontologico"
+                                    type='radio'
+                                    id={`inline-radio-1`}
+                                />
+                                <Form.Check
+                                    inline
+                                    label="Não"
+                                    name="planoOdontologico"
+                                    type='radio'
+                                    id={`inline-radio-2`}
+                                />
+                            </div>
+                        </Form.Group>
+                    </Row>
+                    <hr/>
+                    <Button variant="primary" type="submit" style={{ marginTop: '10px', float: 'right', marginLeft: '10px' }}>
                         Submit
                     </Button>
+                    <Link to="/"><Button style={{ marginTop: '10px', float: 'right' }} variant="secondary">Voltar</Button></Link>&nbsp;
                 </Form>
-            <hr></hr>
-            <Button style={{ marginTop: '10px', float: 'right' }} variant="secondary">Salvar</Button>{' '}
             </Container>
         </>
     );
--- src/components/Associates/AssociateList.js
+++ src/components/Associates/AssociateList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Container, Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AssociateHorizontalCard from './AssociateHorizontalCard';
+import { Link } from 'react-router-dom';
 
 class AssociateList extends Component {
   render() {
@@ -12,7 +13,7 @@ class AssociateList extends Component {
                 <hr></hr>
                 <AssociateHorizontalCard></AssociateHorizontalCard>
             <hr></hr>
-            <Button style={{ marginTop: '10px', float: 'right' }} variant="secondary">Cadastrar Associado</Button>{' '}
+            <Link to="/associate/form"><Button to="/associate/form" style={{ marginTop: '10px', float: 'right' }} variant="secondary">Cadastrar Associado</Button></Link>
             </Container>
         </>
     );
