ff4facf44a44ce64b9ed22c3909c6c041b369d99
Kayan Lira <kbrandaolira@gmail.com>
2022-02-23 10:07:51 -0300

    tela de exames semi integrada

--- src/components/Associates/Exams/ExamList.js
+++ src/components/Associates/Exams/ExamList.js
@@ -4,9 +4,59 @@ import AssociateVerticalCard from '../AssociateVerticalCard'
 import ExamCard from './ExamCard'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import { properties } from '../../../properties'
 
 class ExamList extends Component {
+    constructor(){
+        super();
+        this.state = {
+            isLoaded: false,
+            exams: []
+        }
+      }
+    
+      //TODO Passar o ID do associado ao invés de fixo!!!
+      componentDidMount() {
+        fetch(properties.ms_documentos_url + "document/MEDICAL_EXAM/from/ASSOCIATE/1/")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    exams: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                });
+            }
+        )
+      }
+
+
   render() {
+    const { isLoaded, exams } = this.state;
+
+    if(!isLoaded){
+        return <div style={{textAlign: "center"}}>Loading...</div>
+    } else {
+        var body
+
+        if(exams.length > 0){
+            body =  <div className='container-fluid d-flex flex-wrap'>
+                        {exams.map(exam => {
+                            return (
+                                <ExamCard key={exam.id} associate={exam}></ExamCard>
+                            );
+                        })}
+                    </div>
+        } else {
+            body = "Nenhum exame foi encontrado."
+        }
+    }
+
+
     return (
         <>
             <Container>
@@ -15,7 +65,7 @@ class ExamList extends Component {
                     <Col xs={8}>
                         <h4>Exames</h4>
                         <hr></hr>
-                        <ExamCard/>
+                        {body}
                     </Col>
                 </Row>
                 <Row>
--- src/properties.js
+++ src/properties.js
@@ -1,3 +1,4 @@
 export const properties ={
     ms_associados_url: "http://localhost:8090/",
+    ms_documentos_url: "http://localhost:8091/"
 }
